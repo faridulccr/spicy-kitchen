7e67e9f6f2b041c2f5db341d7432181f04d22f68
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-05-10 23:33:25 +0600

    update some feature

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
                 {navigation.state === "loading" ? (
                     <div
                         className="d-flex justify-content-center align-items-center"
-                        style={{ height: "70vh" }}
+                        id="pending"
                     >
                         <Spinner />
                     </div>
