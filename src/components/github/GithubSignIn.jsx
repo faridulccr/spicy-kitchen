d4512d283d53d8c0ba8114c9c997c138a1c608e3
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-05-26 18:37:39 +0600

    update readme file

--- src/components/github/GithubSignIn.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useAuth } from "../../providers/AuthProvider";
-
-const GithubSignIn = () => {
-    const { githubSignIn } = useAuth();
-    return <div>GithubSignIn</div>;
-};
-
-export default GithubSignIn;
--- src/components/google/GoogleSignIn.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useAuth } from "../../providers/AuthProvider";
-
-const GoogleSignIn = () => {
-    const { googleSignIn } = useAuth();
-    return <div>GoogleSignIn</div>;
-};
-
-export default GoogleSignIn;
